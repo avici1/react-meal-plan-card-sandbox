582e0cce5da21c67d880ebbfabefa5d7009d1641
Alain Christian <48126545+gal1l0@users.noreply.github.com>
2021-05-16 21:01:08 +0200

    Added context to the menu component added meal number context to the component

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,11 @@
+import { useContext, useEffect } from "react";
+import { MealNumber } from "./context";
+
 export default () => {
+  const [getMealNumber, setMealNumber] = useContext(MealNumber);
+  useEffect(() => {
+    console.log(`menu ${getMealNumber.target}`);
+    console.log(`menu 1  ${getMealNumber.current}`);
+  }, [getMealNumber]);
   return <div> Menu</div>;
 };
